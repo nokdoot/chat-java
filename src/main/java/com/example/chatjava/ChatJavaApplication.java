4d8520c60c0d20ebe068b3895f9769d2891fc25c
nokdoot <nokdoot@naver.com>
2024-12-08 18:23:00 +0900

    first commit

---
+++ src/main/java/com/example/chatjava/ChatJavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.chatjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ChatJavaApplication.class,
+                args);
+    }
+
+}
---
+++ src/test/java/com/example/chatjava/ChatJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
